2035e4afa3015d02565d4db63ebc3f5f7567f472
Mathews Alufwani <mathewalufwani@gmail.com>
2020-07-06 04:34:30 +0300

    Add news source model clas

---
+++ app/models/news.py
@@ -0,0 +1,11 @@
+class News:
+    '''
+    News class to define Source Objects
+    '''
+
+    def __init__(self,name,description,url,category,language):
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+        self.language = language
