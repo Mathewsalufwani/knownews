a1fa2354252df4a8b9bc1a4debd5afc6f9c840ac
Mathews Alufwani <mathewalufwani@gmail.com>
2020-07-06 15:50:51 +0300

    add title

--- app/main/views.py
+++ app/main/views.py
@@ -9,13 +9,15 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    title = "Knownews"
     popular_news = get_news()
-    return render_template('index.html', popular = popular_news)
+    return render_template('index.html', popular = popular_news, title=title)
 
 @main.route('/articles/<source>')
 def articles(source):
     '''
     View articles from source
     '''
+    title = "Articles"
     articles = get_articles(source)
-    return render_template('articles.html', articles=articles)
+    return render_template('articles.html', articles=articles, title=title)
